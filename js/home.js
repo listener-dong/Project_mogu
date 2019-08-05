a77160f0b3e0e7e55c9ee7ec5a276f1d1b4d751f
cxd_10 <839823170@qq.com>
2019-08-05 21:09:45 +0800

    首页商品楼层

--- js/home.js
+++ js/home.js
@@ -279,7 +279,6 @@ $(function () {
                 return `<li class="goods"><div class="img"><img src=${ele.src}></div><p class="title">${ele.title}</p><p class="price"><span class="original_price">￥${ele.original_price}</span><del class="sale_price">￥${ele.sale_price}</del></p></li>`;
             }).join("");
             this.slider_jt = $("<div></div>").addClass("jiant").html("<span class='before'></span><span class='after'></span>");
-            // this.slider_ul = `<ul class="slider_goods">${slider_lis}</ul>`;
             this.slider_ul = $("<ul></ul>").addClass("slider_goods").html(slider_lis);
             this.slider_cont = $("<div></div>").addClass("slider_content").html(this.slider_ul);
             this.oRight = $("<div></div>").addClass("slider");
@@ -304,7 +303,7 @@ $(function () {
                 left: -this.times * 920 + "px"
             });
         }
-        上一张
+        //上一张
         before() {
             this.times--;
             //临界值检查，到最后一张后往第一张跳
@@ -345,10 +344,21 @@ $(function () {
             // console.log(this.title, this.data);
             this.oContent = null;
             this.oClothes = null;
+            this.oUl = null;
+            this.timer = null;
+            this.goodsSlider = null;
+            this.oJt = null;
+            this.times = 0;
+            this.index = this.data.length; //商品总数
+            this.liWith = $(this.slider_ul).children().eq(0).innerWidth(); //每个商品的宽度
+            this.page = (this.index / 4); //页码
         }
         //初始化
         init() {
             this.createHtml();
+            this.autoPlayer();
+            this.addMouse();
+            this.clickDirection();
         }
         //创建页面标签
         createHtml() {
@@ -376,8 +386,7 @@ $(function () {
             //商品楼层右侧详情
             let goodsDetails = $("<div></div>").addClass("goods_details");
             //商品楼层右侧轮播图
-            let goodsSlider = this.createSlider();
-
+            this.goodsSlider = this.createSlider();
             //商品楼层右侧优惠背景
             let goodsSale = $("<ul></ul>").addClass("goods_sale");
             let sale_lis = this.title.bg_src.map(ele => {
@@ -391,7 +400,7 @@ $(function () {
             }).join("");
             goodsGroup.append(group_lis);
 
-            goodsDetails.append(goodsSlider).append(goodsSale).append(goodsGroup);
+            goodsDetails.append(this.goodsSlider).append(goodsSale).append(goodsGroup);
             oBottom.append(bigPhoto).append(goodsDetails);
             this.oContent.append(oBottom);
         }
@@ -400,12 +409,201 @@ $(function () {
             let html = this.data.map(ele => {
                 return `<li class="goods_list"><img src=${ele.src}><p class="goods_tit">${ele.title}</p><p class="goods_price">￥${ele.sale_price}</p></li>`;
             }).join("");
-            // console.log(html);
-            let oJt = $("<div></div>").addClass("slider_jt").html("<span class='before'></span><span class='after'></span>");
-            let oUl = $("<div></div>").addClass("slider_ul").html(html);
-            return $("<div></div>").addClass("goods_slider").append(oUl).append(oJt);
+            this.oJt = $("<div></div>").addClass("slider_jt").html("<span class='before'></span><span class='after'></span>");
+            this.oUl = $("<ul></ul>").addClass("slider_ul").html(html);
+            return $("<div></div>").addClass("goods_slider").append(this.oUl).append(this.oJt);
+        }
+        //背景图自动轮播
+        autoPlayer() {
+            //定时器
+            this.timer = setInterval(() => {
+                this.after();
+            }, 2000);
+        }
+        //下一张
+        after() {
+            this.times++;
+            //临界值检查，到最后一张后往第一张跳
+            if (this.times >= this.page) {
+                this.oUl.stop().animate({
+                    left: -this.page * 960 + "px"
+                });
+                this.oUl.css("left", 0 + "px"); //跳动有问题
+                this.times = 1;
+            }
+            this.oUl.stop().animate({
+                left: -this.times * 960 + "px"
+            });
+        }
+        //上一张
+        before() {
+            this.times--;
+            //临界值检查，到最后一张后往第一张跳
+            if (this.times < 0) {
+                this.times = this.page - 1;
+                this.oUl.css("left", -(this.times) * 960 + "px");
+                this.oUl.stop().animate({
+                    left: -(4 * 960) + "px"
+                });
+                this.times = this.page - 2;
+            } else {
+                this.oUl.stop().animate({
+                    left: -(this.times * 960) + "px"
+                });
+            }
+        }
+        //鼠标的移入移出事件
+        addMouse() {
+            this.goodsSlider.on("mouseenter", () => {
+                clearInterval(this.timer);
+            })
+            this.goodsSlider.on("mouseleave", () => {
+                this.autoPlayer();
+            })
+        }
+        //鼠标点击箭头上下切换
+        clickDirection() {
+            //拿到页面中上下张切换的标签
+            this.oJt.on("click", (e) => {
+                if (e.target.className == "before") {
+                    this.before();
+                } else if (e.target.className == "after") {
+                    this.after();
+                }
+            })
         }
     }
+    //通用楼层class类
+    class SameManger {
+        constructor(title, data) {
+            this.title = title;
+            this.data = data;
+            this.oBbox = null;
+            this.oContent = null;
+            this.oLen = null;
+            this.oDot = null;
+            this.times = 0;
+        }
+        //初始化
+        init() {
+            this.createHtml();
+            this.autoPlayer();
+            this.clickNumber();
+        }
+        //创建页面表现
+        createHtml() {
+            this.oContent = $("<div></div>").addClass("content"); //动态设置ID
+            this.createHeaer();
+            this.createContent();
+            this.oBbox = $("<div></div>").attr("id", "shoe_bag").append(this.oContent);
+            $("#home").append(this.oBbox);
+        }
+        //创建标题部分标签
+        createHeaer() {
+            let oTitle = $("<div></div>").addClass("same_goods_top").addClass("clear");
+            this.oContent.append(oTitle);
+            let title_tis = this.title.tis.map(ele => {
+                return `<li class="goods_tit_li">${ele}</li>`
+            }).join("");
+            oTitle.html(`<span class='color_block'></span><h2 class='goods_title'>${this.title.title}</h2><ul class='goods_tit_ul'>${title_tis}</ul>`);
+        }
+        //创建内容部分标签
+        createContent() {
+            //商品楼层左侧大图
+            let oBig = this.title.big_photo.map(ele => {
+                return `<li class="big_li"><img src=${ele}></li>`;
+            })
+            let oBig_ul = $("<ul class='big_ul'><ul>").html(oBig);
+            let bigPhoto = $("<div></div>").addClass("big_photo").append(oBig_ul);
+            //商品楼层右侧详情
+            let goodsDetails = $("<div></div>").addClass("goods_details");
+            //详情区域左侧内容
+            let goodsleft = this.cerateGoods();
+            //详情区域右侧内容
+            let goodsright = $("<div></div>").addClass("details_right");
+            let oHot = this.title.hot.map(ele => {
+                return `<li class="hot_li"><img src=${ele.src}><div class="hot_info"><p class="hot_name">${ele.name}</p><p class="hot_price">￥${ele.price}</p></div></li>`;
+            }).join("");
+            let hot_ul = $("<ul class='hot_ul'></ul>").html(oHot);
+            let hot_tit = $("<div></div>").addClass("hot_header").html("<h3 class='hot_title'>热销品推荐</h3><p class='replace'>换一批</p>");
+            goodsright.append(hot_tit).append(hot_ul);
+            goodsDetails.append(goodsleft).append(goodsright);
+            //商品楼层图内容区域标签
+            let oBottom = $("<div></div>").addClass("same_goods_content").addClass("clear");
+            oBottom.append(bigPhoto).append(goodsDetails);
+            this.oContent.append(oBottom);
+        }
+        //详情区域左侧内容
+        cerateGoods() {
+            this.oLen = $("<div></div>").addClass("sleder_len");
+            let oSlider = $("<div></div>").addClass("sleder_content").addClass("clear").append(this.oLen);
+            let oGoodes = $("<div></div>").addClass("details_left");
+            let res = "";
+            let len = this.data.length;
+            for (let i = 0; i < len; i++) {
+                res += `<li class="slider_goods"><img src=${this.data[i].src}><h4 class="slider_title">${this.data[i].title}</h4><p class="slider_price">￥${this.data[i].price}</p></li>`;
+                //每到达6次，输出一次
+                if ((i + 1) % 6 == 0 && res !== "") {
+                    // `<ul class="slider_ul">${res}</ul>`
+                    var ul = $("<ul></ul>").addClass("slider_ul").html(res);
+                    this.oLen.append(ul);
+                    console.log(ul);
+                    res = "";
+                }
+            }
+            let ts = "";
+            for (let j = 0; j < 5; j++) {
+                ts += "<li class='dot'></li>"
+            }
+            this.oDot = $("<ul class='dot_ul clear'></ul>").html(ts);
+            oGoodes.append(oSlider).append(this.oDot);
+            // console.log(oLen.children().length);
+            return oGoodes;
+        }
+        //轮播图自动播放
+        autoPlayer() {
+            setInterval(() => {
+                // this.after()
+            }, 2000);
+        }
+        //下一张
+        after() {
+            this.times++;
+            // console.log(this.times++)
+            if (this.times > this.oLen.children().length - 1) {
+                this.times = 0;
+            }
+            this.oLen.css("left", -this.times * 588 + "px");
+            this.switchNum(this.times);
+        }
+        //上一张
+        before() {
+            this.times--;
+            //临界值检查，到最后一张后往第一张跳
+            if (this.times < 0) {
+                this.times = this.oLen.children().length - 1
+            }
+            this.oPhoto.css("left", -this.times * 588 + "px");
+            this.switchNum(this.times);
+        }
+        //鼠标点击序列切换
+        clickNumber() {
+            let self = this;
+            this.oDot.on("click", "li", function (e) {
+                let index = $(this).index();
+                self.times = index;
+                self.oLen.css("left", -self.times * 588 + "px");
+                self.switchNum(self.times);
+
+            })
+        }
+        //给序列做排他处理
+        switchNum(index) {
+            let lis = this.oDot.children();
+            lis.eq(index).addClass("active").siblings().removeClass("active");
+        }
+    }
+
     //网络请求异步处理
     new Promise(function (resolve, reject) {
         //发送网络请求，banner-nav实例化对象
@@ -441,10 +639,9 @@ $(function () {
             $.ajax({
                 type: "get",
                 url: "./server/panicData.json",
-                // data: "data",
+                data: "data",
                 dataType: "json",
                 success: function (response) {
-
                     let test = new PanicManger(response);
                     test.init();
                     resolve();
@@ -452,27 +649,50 @@ $(function () {
             });
         })
     }).then(function () {
-        $.ajax({
-            type: "get",
-            url: "./server/clothes.json",
-            data: "data",
-            dataType: "json",
-            success: function (response) {
-                let title = response;
-                $.ajax({
-                    type: "get",
-                    url: "./server/clothesGoods.json",
-                    success: function (response) {
-                        /* 模块化测试，测试失败 */
-                        // require(["./js/clothes.js"], function () {
-                        //     let test = new Clothes(res, response);
-                        //     test.init();
-                        // })
-                        let test = new Clothes(title, response);
-                        test.init();
-                    }
-                });
-            }
+        return new Promise(function (resolve) {
+            $.ajax({
+                type: "get",
+                url: "./server/clothes.json",
+                data: "data",
+                dataType: "json",
+                success: function (response) {
+                    let title = response;
+                    $.ajax({
+                        type: "get",
+                        url: "./server/clothesGoods.json",
+                        success: function (response) {
+                            /* 模块化测试，测试失败 */
+                            // require(["./js/clothes.js"], function () {
+                            //     let test = new Clothes(res, response);
+                            //     test.init();
+                            // })
+                            let test = new Clothes(title, response);
+                            test.init();
+                            resolve();
+                        }
+                    });
+                }
+            })
         });
+    }).then(function () {
+        return new Promise(function (resolve) {
+            $.ajax({
+                type: "get",
+                url: "./server/shoeTitle.json",
+                dataType: "json",
+                success: function (response) {
+                    let title = response;
+                    $.ajax({
+                        type: "get",
+                        url: "./server/shoeData.json",
+                        dataType: "json",
+                        success: function (response) {
+                            let test = new SameManger(title, response);
+                            test.init();
+                        }
+                    });
+                }
+            });
+        })
     })
 })
